d54109279ed64c8d702af37c42e493815228736a
JoabFelippx <80433162+JoabFelippx@users.noreply.github.com>
2023-12-05 17:54:27 -0300

    Update main.py sem o menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint, choices
+from datetime import date, timedelta
 from collections import Counter
 
 TIME_NAMES = [
@@ -18,13 +19,11 @@ peso_de_gols = [2, 3, 5, 8, 4, 6, 15, 4, 20, 25, 30]
 with open('nomes.txt', 'r', encoding='utf8') as f:
   NAMES = f.read().splitlines()
 
-
 class Pessoa():
 
   def __init__(self, nome, idade):
     self.nome = nome
     self.idade = idade
-
 
 class Tecnico(Pessoa):
 
@@ -32,7 +31,6 @@ class Tecnico(Pessoa):
     super().__init__(nome, idade)
     self.time_nome = time_nome
     self.carreira = carreira
-
 
 class Jogador(Pessoa):
 
@@ -55,10 +53,10 @@ class Time():
     self.jogadores = []
     self.pontos = 0
 
-    self.add_jogador()
-    self.add_tecnico()
+    self._add_jogador()
+    self._add_tecnico()
 
-  def add_jogador(self):
+  def _add_jogador(self):
     for i in range(11):
         
       j_name = choices(NAMES)
@@ -67,9 +65,9 @@ class Time():
         j_name = choices(NAMES)
         
       self.jogadores.append(
-          Jogador(choices(NAMES), randint(16, 38), POS[i], i + 1, 0))
+          Jogador(j_name, randint(16, 38), POS[i], i + 1, 0))
 
-  def add_tecnico(self):
+  def _add_tecnico(self):
     old_time = TIME_NAMES[randint(0, len(TIME_NAMES) - 1)]
 
     while old_time == self.time_nome:
@@ -78,9 +76,8 @@ class Time():
     self.tecnico = Tecnico(choices(NAMES), randint(30, 65), self.time_nome,
                            'Tecnico ja passou por ' + old_time)
 
-  def add_pontos(self, p):
+  def _add_pontos(self, p):
     self.pontos += p
-
 
 class Rodada():
 
@@ -96,14 +93,14 @@ class Rodada():
     jogos = {i: [] for i in range(1, 39)}
 
     for i in range(1, 39):
+      
       for j in range(10):
+        
         if i < 20:
-          print(f"Rodada {i}: {self.times[j].time_nome} vs {self.times[-1-j].time_nome}")
           jogos[i].append([self.times[j], self.times[-1-j]])
         else:
-          print(f"Rodada {i}: {self.times[-1-j].time_nome} vs {self.times[j].time_nome}")
           jogos[i].append([self.times[-1-j], self.times[j]])
-      print('\n')
+          
       self.times.insert(1, self.times.pop())
     
     self._jogos = jogos
@@ -121,14 +118,14 @@ class Rodada():
           
         vencedor = 'empate'
         # Empate (NOVO, antes tinha outro if pra isso)
-        self._jogos[n][i][0].add_pontos(1)
-        self._jogos[n][i][1].add_pontos(1)
+        self._jogos[n][i][0]._add_pontos(1)
+        self._jogos[n][i][1]._add_pontos(1)
         
       else:
         vencedor = gols.index(max(gols))
 
       if vencedor != 'empate': # pontuação normal para vencedor
-        self._jogos[n][i][vencedor].add_pontos(3)
+        self._jogos[n][i][vencedor]._add_pontos(3)
 
       #Time dentro de casa
       if gols[0] != 0:
@@ -141,7 +138,6 @@ class Rodada():
 
           for obj_jogador in quant_gols_casa:
             if jogador.nome[0] == obj_jogador.nome[0]:
-            #   print(jogador.nome[0], quant_gols_casa[obj_jogador], self._jogos[n][i][0].time_nome)
               jogador.add_gols(quant_gols_casa[obj_jogador])
 
       #Time fora de casa
@@ -153,24 +149,18 @@ class Rodada():
 
           for obj_jogador in quant_gols_fora:
             if jogador.nome[0] == obj_jogador.nome[0]:
-            #   print(jogador.nome[0], quant_gols_fora[obj_jogador], self._jogos[n][i][1].time_nome)
               jogador.add_gols(quant_gols_fora[obj_jogador])
+
+      data =  date.today() + (n * timedelta(7))
 
       confrontos.append(
           Confronto(
               self._jogos[n][i][0].estadio, self._jogos[n][i][0],
               self._jogos[n][i][1],
               f'Resultado: {self._jogos[n][i][vencedor].time_nome} vencedor'
-              if vencedor != 'empate' else vencedor, '01/01/01',
+              if vencedor != 'empate' else vencedor, data,
               f'{self._jogos[n][i][0].time_nome} {gols[0]} X {gols[1]} {self._jogos[n][i][1].time_nome}'
           ))
-
-    # print(confrontos)
-    # for sla in confrontos:
-    #   print(f'Estadio: {sla.estadio}')
-    #   print(sla.placar)
-    #   print(sla.resultado)
-    #   print('\n')
 
     return confrontos
 
@@ -199,9 +189,9 @@ class Brasileirao():
     self.n_rodada = 0
     
     self.__confrontos = {}
-
     self._classificacao = {}
-    self._artilheiros = {}
+    
+    self._jogadoresQntGols = {}
 
     self._add_times()
     self._add_rodada()
@@ -209,18 +199,18 @@ class Brasileirao():
   def _add_times(self):
     for i in range(20):
       self.__times.append(Time(TIME_NAMES[i], TIME_NAMES[i] + ' Arena'))
-    print('Times adicionados com sucesso!')
 
   def _add_rodada(self):
     self.__rodadas = Rodada(self.__times)
-    print('Confrontos criados!')
+
 
   def get_times(self):
     for time in self.__times:
+      
       print(f'Time: {time.time_nome} - Estadio: {time.estadio}')
-      print(
-          f'\tTecnico: {time.tecnico.nome[0]} - Carreira: {time.tecnico.carreira}'
-      )
+      
+      print(f'\tTecnico: {time.tecnico.nome[0]} - Carreira: {time.tecnico.carreira}')
+      
       for jogador in time.jogadores:
         print(
             f'\tJogador: {jogador.nome[0]} - Camisa: {jogador._num_camisa} - Posicao: {jogador.posicao} - Gols: {jogador.gols}'
@@ -231,53 +221,60 @@ class Brasileirao():
 
     self.__confrontos[n_rodada] = self.__rodadas.get_confrontos(n_rodada)
     self.n_rodada = len(self.__confrontos)
-    print(f'Rodada {n_rodada}')
-    for i in self.__confrontos[n_rodada]:
-        print(f'{i.time1.time_nome} {i.placar} {i.time2.time_nome}')
-
-    # for casa in self.__confrontos[n_rodada]:
-    #   print(casa.time1.time_nome, casa.time1.pontos)
-
-    # print('\n')
-    # for fora in self.__confrontos[n_rodada]:
-    #   print(fora.time2.time_nome, fora.time2.pontos)
 
   def get_rodadas(self, n_rodada):
         print(f'Rodada {n_rodada}')
         for i in self.__confrontos[n_rodada]:
             print(f'{i.placar}')
-
+        
   def get_artilheiros(self):
-    pass
+    
+    aux_jogadores_gols = []
+    
+    for time in self.__times:
+    
+      for jogador in time.jogadores:
+     
+        try:
+          self._jogadoresQntGols[time.time_nome].update({jogador.nome[0]: (jogador.gols, time.time_nome)})
+        except:
+          self._jogadoresQntGols[time.time_nome] = {jogador.nome[0]: (jogador.gols, time.time_nome)}
+    
 
+    
+    for time in self._jogadoresQntGols:
+      
+      self._jogadoresQntGols[time] = sorted(self._jogadoresQntGols[time].items(), key=lambda x: x[1], reverse=True)
+      aux_jogadores_gols = aux_jogadores_gols + self._jogadoresQntGols[time]
+      aux_jogadores_gols = sorted(aux_jogadores_gols, key=lambda x: x[1], reverse=True)
+    pos = 1
+    
+    for i in aux_jogadores_gols:  
+    
+      if pos > 5:
+        break
+      
+      print(f'{pos}º {i[0]} - {i[1][0]} gols - {i[1][1]}')
+      pos += 1  
+    
   def get_classificacao(self):
     
     for time in self.__times:
-        try:
-            
-            self._classificacao[time.pontos].append(time.time_nome)
-        except:
-            self._classificacao[time.pontos] = [time.time_nome]
+      try:        
+          self._classificacao[time.pontos].append(time.time_nome)
+      except:
+          self._classificacao[time.pontos] = [time.time_nome]
 
     pos = 1
-    for i in sorted(self._classificacao, reverse=True):
-        for time in self._classificacao[i]:
-            print(f'{pos}ª {time} - {i}')
-            pos += 1
 
+    for i in sorted(self._classificacao, reverse=True):
+      for time in self._classificacao[i]:
+          print(f'{pos}ª {time} - {i}')
+          pos += 1
+  
     self._classificacao = {}
 
 if __name__ == '__main__':
-    
 
   brasileirao = Brasileirao()
-  brasileirao.gerar_rodadas(1)
-  brasileirao.gerar_rodadas(2)
-  brasileirao.gerar_rodadas(3)
-  brasileirao.get_classificacao()
-  brasileirao.gerar_rodadas(4)
-  brasileirao.gerar_rodadas(5)
-  brasileirao.get_classificacao()
-  brasileirao.get_rodadas(1)
-  brasileirao.get_artilheiros()
-
+  
